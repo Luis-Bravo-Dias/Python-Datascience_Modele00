4b857b8367efeb06a956a585bdf9d5f7f1949f0e
Luis miguel Leiria bravo rodrigues dias <lleiria-@c1r5s9.42lisboa.com>
2025-06-24 11:49:54 +0100

    flake8 corrections

--- ex05/building.py
+++ ex05/building.py
@@ -1,54 +1,55 @@
 import sys
 
+
 def print_count(str):
-	"""
-	Displays the sums of total characters, upper-case characters, lower-case characters,
-	punctuation characters, digits and spaces of a string that receives as an argument.
-	"""
-	string = str
-	upper_letters = 0
-	lower_letters = 0
-	p_marks = 0
-	spaces = 0
-	digits = 0
+    """
+    Displays the sums of total characters, upper-case characters,
+    lower-case characters, punctuation characters,
+    digits and spaces of a string that receives as an argument.
+    """
 
-	punctuation = ".,;:!?\"'()]{[}-"
+    string = str
+    upper_letters = 0
+    lower_letters = 0
+    p_marks = 0
+    spaces = 0
+    digits = 0
+    punctuation = ".,;:!?\"'()]{[}-"
 
-	for char in range(len(string)):
-		if string[char].isupper():
-			upper_letters += 1
-		elif string[char].islower():
-			lower_letters += 1
-		elif string[char] in punctuation:
-			p_marks += 1
-		elif string[char].isspace():
-			spaces += 1
-		elif string[char].isdigit():
-			digits += 1
+    for char in range(len(string)):
+        if string[char].isupper():
+            upper_letters += 1
+        elif string[char].islower():
+            lower_letters += 1
+        elif string[char] in punctuation:
+            p_marks += 1
+        elif string[char].isspace():
+            spaces += 1
+        elif string[char].isdigit():
+            digits += 1
 
-	print("The text contains", len(string), "characters:")
-	print(upper_letters, "upper letters")
-	print(lower_letters, "lower letters")
-	print(p_marks, "punctuation marks")
-	print(spaces, "spaces")
-	print(digits, "digits")
+    print("The text contains", len(string), "characters:")
+    print(upper_letters, "upper letters")
+    print(lower_letters, "lower letters")
+    print(p_marks, "punctuation marks")
+    print(spaces, "spaces")
+    print(digits, "digits")
 
 
 def main():
-	argc = len(sys.argv)
+    argc = len(sys.argv)
+    as_error = "AssertionError: more than one argument is provided"
 
-	if (argc > 1):
-		try:
-			assert argc == 2,"AssertionError: more than one argument is provided"
-			print_count(sys.argv[1])
-		except AssertionError as error:
-			print(error)
-	else:
-		print("What is the text to count?...")
-		print_count(input())
-
-		
+    if (argc > 1):
+        try:
+            assert argc == 2, as_error
+            print_count(sys.argv[1])
+        except AssertionError as error:
+            print(error)
+    else:
+        print("What is the text to count?...")
+        print_count(input())
 
 
 if __name__ == "__main__":
-	main()
+    main()
