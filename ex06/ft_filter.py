571d6d3c378e7214afca3e1f22e80c5312216ca0
Luis miguel Leiria bravo rodrigues dias <lleiria-@c1r1s6.42lisboa.com>
2025-06-25 13:58:51 +0100

    ex06 part 1

---
+++ ex06/ft_filter.py
@@ -0,0 +1,15 @@
+def ft_filter(function, iterable):
+    """
+    filter(function or None, iterable) --> filter object
+
+    Return an iterator yielding those items of iterable for
+    which function(item) is true. If function is None, return
+    the items that are true.
+    """
+
+    for item in iterable:
+        if function:
+            if function(item):
+                yield item
+        elif item:
+            yield item
