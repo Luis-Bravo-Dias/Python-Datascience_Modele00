5333e5aed54d284e00645410dee3ed77e3724b58
Luis miguel Leiria bravo rodrigues dias <lleiria-@c1r1s4.42lisboa.com>
2025-06-26 17:23:12 +0100

    ex06 part 2 start

--- ex06/filterstring.py
+++ ex06/filterstring.py
@@ -1,1 +1,19 @@
+from ft_filter import ft_filter
+import sys
+
 print(filter.__doc__)
+
+def main():
+    argc = len(sys.argv)
+    as_error = "AssertionError: more than one argument is provided"
+
+    if (argc > 1):
+        try:
+            assert argc == 2, as_error
+           
+        except AssertionError as error:
+            print(error)
+
+
+if __name__ == "__main__":
+    main()
