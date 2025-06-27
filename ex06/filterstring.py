8be94453e5ed10538a239a2a83202bf985c82006
Luis miguel Leiria bravo rodrigues dias <lleiria-@c1r1s4.42lisboa.com>
2025-06-27 15:02:56 +0100

    ex06 part 2

--- ex06/filterstring.py
+++ ex06/filterstring.py
@@ -1,18 +1,27 @@
 from ft_filter import ft_filter
 import sys
 
-print(filter.__doc__)
 
 def main():
     argc = len(sys.argv)
-    as_error = "AssertionError: more than one argument is provided"
+    as_error = "AssertionError: the arguments are bad"
 
-    if (argc > 1):
-        try:
-            assert argc == 2, as_error
-           
-        except AssertionError as error:
-            print(error)
+    try:
+        assert argc == 3, as_error
+        S = sys.argv[1]
+        N = int(sys.argv[2])
+        assert all(word.isalnum() for word in S.split()), as_error
+
+        first_list = [word for word in S.split()]
+        new_list = list(ft_filter(lambda x: len(x) > N, first_list))
+        #print(first_list)
+        print(new_list)
+        #print(filter.__doc__)
+        #print(ft_filter.__doc__)
+    except ValueError:
+        print(as_error)
+    except AssertionError as error:
+        print(error)
 
 
 if __name__ == "__main__":
