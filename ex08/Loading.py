730a618af63fc96323d5f950378621b3726523b5
Luis miguel Leiria bravo rodrigues dias <lleiria-@c1r1s6.42lisboa.com>
2025-08-31 13:41:03 +0100

    ex 08

--- ex08/Loading.py
+++ ex08/Loading.py
@@ -1,6 +1,9 @@
 def ft_tqdm(lst: range) -> None:
-    total = len(range)
-    for i, item in enumerate(range, 1):
+    total = len(lst)
+    bar_len = 25
+    for i, item in enumerate(lst, 1):
         percent = int (i / total * 100)
-        bar = "=" * 
-    return 0
+        bar = "=" * (percent // 2) + ">" + " " * (bar_len - percent // 2)
+        print(f"\r{percent}%|{bar}| {i}/{total}", end="", flush=True)
+        yield item
+    print()
