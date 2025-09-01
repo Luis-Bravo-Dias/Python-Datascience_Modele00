fa5492fc9333d7fc6708e73b654345523b00975c
Luis miguel Leiria bravo rodrigues dias <lleiria-@c1r1s6.42lisboa.com>
2025-09-01 14:51:55 +0100

    ex08 corretion

--- ex08/Loading.py
+++ ex08/Loading.py
@@ -1,9 +1,15 @@
 def ft_tqdm(lst: range) -> None:
+    """
+    Decorate an iterable object, returning an iterator which acts exactly
+    like the original iterable, but prints a dynamically updating
+    progressbar every time a value is requested.
+    """
     total = len(lst)
-    bar_len = 25
+    bar_len = 61
     for i, item in enumerate(lst, 1):
-        percent = int (i / total * 100)
-        bar = "=" * (percent // 2) + ">" + " " * (bar_len - percent // 2)
-        print(f"\r{percent}%|{bar}| {i}/{total}", end="", flush=True)
+        percent = int(i / total * 100)
+        filled = (percent * bar_len) // 100
+        bar = "=" * (filled - 1) + ">" + " " * (bar_len - filled)
+        print(f"\r{percent}%[{bar}] {i}/{total}", end="", flush=True)
         yield item
     print()
