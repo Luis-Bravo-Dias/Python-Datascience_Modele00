f671289826d97e67be5b6693f9a46319d1dd5f70
Luis miguel Leiria bravo rodrigues dias <lleiria-@c2r4s8.42lisboa.com>
2025-09-18 18:10:14 +0100

    ex09

---
+++ ex09/src/ft_package/__init__.py
@@ -0,0 +1,15 @@
+from .ft_package import (
+    print_count,
+    ft_filter,
+    decoder,
+    ft_tqdm,
+    count_in_list,
+)
+
+__all__ = [
+    "print_count",
+    "ft_filter",
+    "decoder",
+    "ft_tqdm",
+    "count_in_list",
+]
